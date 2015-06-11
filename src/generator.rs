afdc734f72892b7f9a1601bdd3857da7d7150fd6
Jim Stanton <jim@jhstanton.com>
2015-06-11 01:20:17 -0500

    Debugging various tree items, so lots of print statements in maze generator here

--- src/generator.rs
+++ src/generator.rs
@@ -4,8 +4,8 @@ extern crate std;
 use rand::Rng;
 use std::cmp;
 
-pub struct Maze<'a> {
-    start: InitialRoom<'a>,
+pub struct Maze {
+    start: InitialRoom,
     maze : MazePath
 }
 
@@ -17,9 +17,9 @@ pub struct Exit {
     locked: bool
 }
 
-pub struct InitialRoom<'a> {
-    exit    : &'a Door, 
-}
+pub struct InitialRoom;
+//    exit    : Door, 
+//}
 
 pub enum MazePath{
     Room {
@@ -66,22 +66,37 @@ impl MazePath {
         fn build(additional_rooms: u32, exit_here: bool, max_rooms_attached: &u32) -> MazePath {
             if additional_rooms == 0 {
                 if exit_here {
+                    println!("Exit here!");
                     MazePath::Exit{ entrance: Door::new(), exit: Exit::new() }
                 }
                 else {
+                    println!("made a room");
                     MazePath::Room { door: Door::new() }
                 }
             }
             else {
-                let attached_room_count = rand::thread_rng().gen_range(1, std::cmp::min(additional_rooms, max_rooms_attached + 1));
+                let attached_room_count = rand::thread_rng().gen_range(0, std::cmp::min(additional_rooms, *max_rooms_attached) + 1);
                 let branch_with_exit = rand::thread_rng().gen_range(0, attached_room_count + 1); // gen_range is exclusive on the RHS, hence the + 1
+                println!("Branch with exit: {}", branch_with_exit);
                 let mut rooms_left = additional_rooms - attached_room_count;
                 let mut rooms_here = 0;
                 
-                MazePath::Connector{ door: Door::new(), other_rooms: { 
+                MazePath::Connector{ door: Door::new(), other_rooms: {
                     let mut rooms = vec![];
+                    println!("Attached room count: {}", attached_room_count);
                     for room_num in (0..attached_room_count + 1) {
-                        rooms_here = rand::thread_rng().gen_range(0, rooms_left - (attached_room_count + 1));
+                        println!("Rooms left to generate: {}", rooms_left);
+
+                        if room_num == branch_with_exit { println!("Making exit branch") }
+                        rooms_here = 
+                            if room_num == attached_room_count {
+                                println!("At end of connector branch, rooms_here: {}", rooms_left);
+                                rooms_left  
+                            }
+                            else {
+                                rand::thread_rng().gen_range(0, rooms_left + 1)
+                            };
+                            
                         rooms_left -= rooms_here;
                         rooms.push(build(rooms_here, exit_here && room_num == branch_with_exit, max_rooms_attached))
                     }
@@ -94,4 +109,8 @@ impl MazePath {
     
 }
 
-
+impl Maze {
+    pub fn new(num_rooms: u32) -> Maze {
+        Maze { start: InitialRoom, maze: MazePath::new(num_rooms) }
+    }
+}
